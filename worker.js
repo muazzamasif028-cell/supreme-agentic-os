dd0d5b1b7e063ba0600df45017add257d5501699
muazzamasif028-cell <muazzamasif028@gmail.com>
2026-05-17 08:22:24 -0700

    Implement Worker class to process jobs from queue

--- worker.js
+++ worker.js
@@ -1,1 +1,29 @@
+const jobQueue = require('./job.queue');
+const processor = require('./processor');
+
+class Worker {
+    constructor() {
+        this.active = false;
+        this.processedCount = 0;
+    }
+    
+    start() {
+        this.active = true;
+        console.log('👷 Worker started');
+        
+        jobQueue.on('job:added', async (job) => {
+            if (this.active) {
+                await processor.process(job);
+                this.processedCount++;
+            }
+        });
+    }
+    
+    stop() {
+        this.active = false;
+        console.log(`👷 Worker stopped. Processed: ${this.processedCount}`);
+    }
+}
+
+module.exports = new Worker();
 
